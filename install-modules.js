1f21c429dc29491f5e778044834417d63ac1c96f
Lucas Blanc <78850304+Lucxs95@users.noreply.github.com>
2023-06-08 10:49:51 +0200

    test

---
+++ install-modules.js
@@ -0,0 +1,13 @@
+const { execSync } = require('child_process');
+
+try {
+    // Install express
+    execSync('npm install express');
+
+    // Install mqtt
+    execSync('npm install mqtt');
+
+    console.log('Modules installed successfully.');
+} catch (error) {
+    console.error('Error occurred while installing modules:', error.message);
+}
